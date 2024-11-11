e79a57de3deb16963cad8a14d628807f5ebb13a7
khader huijih <164516684+khaderGg@users.noreply.github.com>
2024-11-11 14:43:28 +0300

    finishserves

--- lib/helper/apire.dart
+++ lib/helper/apire.dart
@@ -4,8 +4,15 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 class api {
-  Future<dynamic> Get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  Future<dynamic> Get({required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(
+      Uri.parse(url),
+      headers: headers,
+    );
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
     } else {
@@ -25,7 +32,34 @@ class api {
     http.Response responss = await http.post(Uri.parse(uri),
         body: body, headers: headers //https://fakestoreapi.com/products
         );
-    Map<String, dynamic> data = jsonDecode(responss.body);
-    return data;
+    if (responss.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(responss.body);
+      return data;
+    } else {
+      throw Exception(
+          "exception ${responss.statusCode} , body ${jsonDecode(responss.body)}");
+    }
+  }
+
+  Future<dynamic> Put(
+      {required String uri,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    http.Response responss = await http.post(Uri.parse(uri),
+        body: body, headers: headers //https://fakestoreapi.com/products
+        );
+    if (responss.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(responss.body);
+      return data;
+    } else {
+      throw Exception(
+          "exception ${responss.statusCode} , body ${jsonDecode(responss.body)}");
+    }
   }
 }
